493d0d9894be394e7f12a83b5eabf95f72823239
roholly <rosemary.holly@gmail.com>
2016-02-29 14:18:51 -0600

    Update PlayingCard.java

--- PlayingCard.java
+++ PlayingCard.java
@@ -5,13 +5,13 @@ public class PlayingCard {
 	private String rank;
 	private String suit;
 	private static String[] ranks = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King", "Ace"};
-	private static String[] suits = {"Club", "Diamond", "Heart", "Spade"};
+	private static String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
 		
 	public PlayingCard(String r, String s) {
 		System.out.println("In PlayingCard(int r, int s) constructor.");
 		this.rank = r;
 		this.suit = s;
-		System.out.println("Just created a PlayingCard:  " + this.rank + " of " + this.suit + "s.");
+		System.out.println("Just created a PlayingCard:  " + this.rank + " of " + this.suit);
 	}
 
 		public String getRank(int ranksElement) {
@@ -33,11 +33,14 @@ public class PlayingCard {
 	}
 	
 	public static void main(String[] args) {
-		PlayingCard cardOne = new PlayingCard("3","Club");
-		for (String rank: ranks) {
+		PlayingCard cardOne = new PlayingCard("3","Clubs");
+		for (String suit: suits) {
+			for (String rank: ranks) {
+				System.out.println(rank + " of " + suit);
+			}
 		}
 			
-		}
 	}
+
 
 }
