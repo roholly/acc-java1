4d4606b6184c24d530ac35e3dad60ed539080d65
roholly <rosemary.holly@gmail.com>
2016-02-29 21:43:41 -0600

    Update PlayingCard.java

--- PlayingCard.java
+++ PlayingCard.java
@@ -1,21 +1,31 @@
 package edu.acc.java;
+import java.util.Random;
 
-public class PlayingCard {
+public class PlayingCard implements PlayingCardIF {
 	
 	private String rank;
 	private String suit;
-	private static String[] ranks = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King", "Ace"};
-	private static String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+	//Consider making the following "final" since you know those values won't change.
+	final static String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+	final static String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
 		
 	public PlayingCard(String r, String s) {
-		System.out.println("In PlayingCard(int r, int s) constructor.");
+		System.out.println("In PlayingCard(String r, String s) constructor.");
 		this.rank = r;
 		this.suit = s;
 		System.out.println("Just created a PlayingCard:  " + this.rank + " of " + this.suit);
 	}
 
-		public String getRank(int ranksElement) {
-			this.rank = this.ranks[ranksElement];
+	public PlayingCard() {
+		// no rank or suit given so randomize a card
+		Random rand = new Random();
+		this.suit = SUITS[rand.nextInt(SUITS.length)];
+		this.rank = RANKS[rand.nextInt(RANKS.length)];
+		System.out.println("Just created a PlayingCard:  " + this.rank + " of " + this.suit);
+	}
+	
+	public String getRank(int ranksElement) {
+			this.rank = PlayingCard.RANKS[ranksElement];
 			return this.rank;
 	}
 
@@ -24,7 +34,7 @@ public class PlayingCard {
 	}
 
 	public String getSuit(int suitsElement) {
-		this.suit = this.suits[suitsElement];
+		this.suit = PlayingCard.SUITS[suitsElement];
 		return this.suit;
 	}
 
@@ -32,15 +42,23 @@ public class PlayingCard {
 		this.suit = suit;
 	}
 	
+	@Override
+	// System.out.println(objectName) will reference this implicitly.
+	public String toString() {
+		return this.rank + " of " + this.suit;
+	}
+	
 	public static void main(String[] args) {
 		PlayingCard cardOne = new PlayingCard("3","Clubs");
-		for (String suit: suits) {
-			for (String rank: ranks) {
+		System.out.println(cardOne);
+		PlayingCard cardTwo = new PlayingCard();
+		System.out.println(cardTwo);
+		for (String suit: SUITS) {
+			for (String rank: RANKS) {
 				System.out.println(rank + " of " + suit);
 			}
 		}
 			
 	}
-
 
 }
