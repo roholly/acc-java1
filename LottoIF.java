923466e65dc284a2cc6d2bad16975e1f8a87bf67
roholly <rosemary.holly@gmail.com>
2016-04-04 16:39:50 -0500

    Create LottoIF.java

---
+++ LottoIF.java
@@ -0,0 +1,12 @@
+package edu.acc.java;
+
+public interface LottoIF {
+	
+	static final int NUM_LOTTO_BALLS = 6;
+	static final int MAX_BALL_NUM = 54;
+	static final int YEARS = 1;
+	void calc_odds();
+	void print_results();
+	
+
+}
